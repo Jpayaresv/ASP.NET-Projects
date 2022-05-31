a7012335a58bb301bb898f5ca134e955ec071c06
Jose Payares <jpayaresv06@gmail.com>
2022-05-30 22:17:45 -0500

    ..

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -16,27 +16,29 @@ namespace ASP.NET_Core.Controllers
         }
         public IActionResult MultiAlumno()
         {
-
-            var listaAlumnos = new List<Alumno>(){
-                            new Alumno{Nombre="Matemáticas",
-                                UniqueId= Guid.NewGuid().ToString()
-                            } ,
-                            new Alumno{Nombre="Educación Física",
-                                UniqueId= Guid.NewGuid().ToString()
-                            },
-                            new Alumno{Nombre="Castellano",
-                                UniqueId= Guid.NewGuid().ToString()
-                            },
-                            new Alumno{Nombre="Ciencias Naturales",
-                                UniqueId= Guid.NewGuid().ToString()
-                            }
-                            ,
-                            new Alumno{Nombre="Programación",
-                                UniqueId= Guid.NewGuid().ToString()
-                            }
-                };
+            
+            var listaAlumnos = GenerarAlumnosAlAzar();
         
             return View("MultiAlumno",listaAlumnos);
         }
+
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { 
+                                   Nombre = $"{n1} {n2} {a1}" ,
+                                   UniqueId = Guid.NewGuid().ToString()
+                                   };
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
+        }
+
+
     }
 }
